60b323cc9cb210466fa904765804528546fb6bc4
Thiago Bianeck <76410241+thgbianeck@users.noreply.github.com>
2025-07-08 23:33:13 -0300

    Initial commit

---
+++ src/test/java/com/viasoft/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viasoft.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
